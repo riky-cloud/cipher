f5bd64a2d8fd3c1d019aa7c1d8a07cf2e4d37650
Riky Cloud <rikycloud@gmail.com>
2016-10-04 11:07:15 +0700

    code

---
+++ set.php
@@ -0,0 +1,78 @@
+<?php
+	
+	function cipher()
+	{	
+		// digit 5 for str_split by 5 char
+		$rand = 1000;
+		$i = $rand + 9989;
+
+		foreach (range('A', 'Z') as $char) {
+		    $data[$i] =  $char;
+
+		    $i = $i + $rand;
+
+		    $rand++;
+		}
+
+		foreach (range('!', '@') as $char) {
+		    $data[$i] =  $char;
+
+		    $i = $i + $rand;
+
+		    $rand++;
+		}
+
+		foreach (range('a', 'z') as $char) {
+		    $data[$i] =  $char;
+
+		    $i = $i + $rand;
+
+		    $rand++;
+		}
+		$i++;
+		$data[$i] = ' ';
+		$data[$i] = '_';
+
+		return $data;
+	}
+
+	function cipher_encode($text, $data)
+	{
+		$arrText = str_split($text);
+
+		foreach ($arrText as $tx) {
+			if(in_array($tx, $data)) {
+				$arrKey[] 	= array_search($tx, $data);
+			} 
+		}
+
+		return join($arrKey);
+	}
+
+	function cipher_decode($encode, $data)
+	{
+		$arrKey = str_split($encode, 5);
+		foreach ($arrKey as $key) {
+			$result[] = $data[$key];
+		}
+
+		return join($result);
+	}
+
+
+	$data = cipher();
+	$text = 'hello';
+	$encode = cipher_encode($text, $data);
+	$decode = cipher_decode($encode, $data);
+
+	echo '<pre>';
+	echo "Base array";
+	print_r($data);
+	echo '</pre>';
+
+	echo "text => hello <br>";
+	echo 'encode => '. $encode;
+	echo "<br>";
+	echo 'decode => '.$decode;
+
+?>
